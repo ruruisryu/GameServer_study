8a5fda826e3a3650a98b6c8286e519c4df6aa83f
Seohyun Ryu <79790536+Ryuryu128@users.noreply.github.com>
2023-11-06 23:58:32 +0900

    Section1. 쓰레드 생성

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Threading;
 
-Console.WriteLine("Hello, World!");
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("Hello Thread!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+            
+            for (int i = 0; i < 5; i++)
+            {
+                // TaskCreationOptions.LongRunning: 이 Task는 아주 오래 걸리는 작업임을 명시해줌으로써, Threadpool의 쓰레드를 차지하지않고 별도로 실행되도록 해줌.
+                Task t = new Task(() => {while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+           
+            // 정상적으로 실행됨
+            ThreadPool.QueueUserWorkItem(MainThread);
+        }
+    }
+}
