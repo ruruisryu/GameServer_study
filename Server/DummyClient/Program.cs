7f62c08344cd042471864c7a8cb0fbba6de4a0ef
Seohyun Ryu <79790536+Ryuryu128@users.noreply.github.com>
2023-11-27 01:44:57 +0900

    C# TCP기반 소켓 프로그래밍 기초 구현
    루키스님의 강의 [C#과 유니티로 만드는 MMORPG 게임 개발 시리즈] Part4: 게임 서버의 "소켓 프로그래밍 기초" 부분을 수강하며 짠 코드입니다.

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS (Domain Name System)
+            // 로컬 컴퓨터의 ip를 가져와 도메인으로 변환한 뒤 변환 주소를 ipAdd에 저장
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAdd = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAdd, 7777);  // IpAddress와 포트 넘버를 인자로 넘겨줌
+            
+            // 소켓 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 서버 소켓에 connect 문의
+                socket.Connect(endPoint);
+                // 연결된 서버의 EndPoint를 출력
+                Console.WriteLine($"connected To {socket.RemoteEndPoint.ToString()}");
+            
+                // 정보를 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World !");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 정보를 받는다
+                byte[] recvBuff = new byte[1024];
+                int byteCount = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, byteCount);
+                Console.WriteLine($"[From Server] {recvData}");
+            
+                // 나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+    }
+}
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,31 +1,63 @@
-﻿using System.Threading;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
-        {
-            for(int i=0; i<5; i++)
-            {
-                Console.WriteLine("Hello Thread!");
-            }
-        }
-
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            // DNS (Domain Name System)
+            // 로컬 컴퓨터의 ip를 가져와 도메인으로 변환한 뒤 변환 주소를 ipAdd에 저장
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAdd = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAdd, 7777);  // IpAddress와 포트 넘버를 인자로 넘겨줌
             
-            for (int i = 0; i < 5; i++)
+            // Listner(문지기) 생성
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            
+            // Bind -> 문지기 교육
+            // 서버의 Ip주소와 포트 넘버 정보를 가지고 있는 EndPoint를 넣어줌
+            listenSocket.Bind(endPoint);
+            
+            // Listen -> 영업 시작
+            // 인자 backLog: 최대 대기수
+            listenSocket.Listen(10);
+
+            try
             {
-                // TaskCreationOptions.LongRunning: 이 Task는 아주 오래 걸리는 작업임을 명시해줌으로써, Threadpool의 쓰레드를 차지하지않고 별도로 실행되도록 해줌.
-                Task t = new Task(() => {while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+                
+                    // Accept -> 손님을 입장시킨다.
+                    // Accept는 클라이언트의 소켓을 반환
+                    Socket clientSocket = listenSocket.Accept();
+                
+                    // 정보를 받는다
+                    byte[] recvBuff = new byte[1024];                   // 정보를 받을 버퍼
+                    int recvBytes = clientSocket.Receive(recvBuff);     // 받은 정보의 바이트수
+                    // 받은 정보(바이트)를 string으로 인코딩
+                    // 인자 세가지는 인코딩할 바이트 배열, 시작 인덱스, 바이트 개수
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes); 
+                    Console.WriteLine($"[From Client]: {recvData}");
+
+                    // 정보를 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG server !");
+                    clientSocket.Send(sendBuff);
+                
+                    // 쫓아낸다
+                    // clientSocket.Shutdown(SocketShutdown.Both);  // 우아하게 예고
+                    clientSocket.Close();   // 쫓아내기
+                }
             }
-           
-            // 정상적으로 실행됨
-            ThreadPool.QueueUserWorkItem(MainThread);
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
